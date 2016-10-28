4773815656195ab84632c58c8bf74ac3e0f9b1d1
Satchel Grant <grantsrb@gmail.com>
2016-10-28 01:20:58 -0700

    add question model

--- app/models/question.js
+++ app/models/question.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  author: DS.attr(),
+  content: DS.attr(),
+  tag: DS.attr(),
+  date: DS.attr(),
+  // answers: DS.hasMany("answer")
 });
